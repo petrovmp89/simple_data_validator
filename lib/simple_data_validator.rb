dd3f2098ef71fc59773d4037494093ba96bd4e15
Maksim Petrov <petrovmp89@gmail.com>
2018-04-02 22:53:43 +0300

    Add ruby version

--- lib/simple_data_validator.rb
+++ lib/simple_data_validator.rb
@@ -5,22 +5,21 @@ module SimpleDataValidator
 
   def validate_all_data(report_name: DEFAULT_REPORT_NAME)
     CSV.open(report_name, 'wb') do |csv|
-        ActiveRecord::Base.descendants.each do |model|
-          begin
-            invalid_records = model.select(&:invalid?)
-            csv << [model, [invalid_records.count, 'invalid records found'].join(' ')]
-            invalid_records.each do |record|
-              begin
-                csv << ['', ['id=',record.id].join, record.errors.full_messages.to_sentence]
-              rescue => e
-                csv << ['', ['id=',record.id].join, e.message]
-                next
-              end
+      ActiveRecord::Base.descendants.each do |model|
+        begin
+          invalid_records = model.select(&:invalid?)
+          csv << [model, [invalid_records.count, 'invalid records found'].join(' ')]
+          invalid_records.each do |record|
+            begin
+              csv << ['', ['id=',record.id].join, record.errors.full_messages.to_sentence]
+            rescue => e
+              csv << ['', ['id=',record.id].join, e.message]
+              next
             end
-          rescue => e
-            csv << [model, e.message]
-            next
           end
+        rescue => e
+          csv << [model, e.message]
+          next
         end
       end
     end
